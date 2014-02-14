c8404ac4845925a748c0ec606cde998cf10c7459
bikerglen <glen@glen-mbpro1.local>
2014-02-14 08:27:54 -0700

    Initial checkin.

---
+++ projects/led-panel-v01/config-scripts/setup_device-tree-slow.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+rm /lib/firmware/BB-BONE-LOGIBONE-SLOW.dtbo
+dtc -O dtb -o BB-BONE-LOGIBONE-SLOW.dtbo -b 0 -@ BB-BONE-LOGIBONE-SLOW.dts
+cp BB-BONE-LOGIBONE-SLOW.dtbo /lib/firmware
+sh -c "echo -4 > /sys/devices/bone_capemgr.8/slots "
+sh -c "echo -5 > /sys/devices/bone_capemgr.8/slots "
+sh -c "echo -6 > /sys/devices/bone_capemgr.8/slots "
+sh -c "echo BB-BONE-LOGIBONE:SLOW > /sys/devices/bone_capemgr.8/slots "
+cat  /sys/devices/bone_capemgr.8/slots
+insmod logibone_ra2_dm.ko
