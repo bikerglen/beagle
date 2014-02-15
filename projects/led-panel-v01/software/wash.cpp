c36f21b0bc642c1d9a1ec0a32d4f9ec26ab07a84
bikerglen <glen@glen-mbpro1.local>
2014-02-14 22:23:52 -0700

    Initial checkin.

---
+++ projects/led-panel-v01/software/wash.cpp
@@ -0,0 +1,104 @@
+//=============================================================================================
+// LED Matrix Animated Pattern Generator
+// Copyright 2014 by Glen Akins.
+// All rights reserved.
+// 
+// Set editor width to 96 and tab stop to 4.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//=============================================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+#include "globals.h"
+#include "pattern.h"
+#include "wash.h"
+
+
+//---------------------------------------------------------------------------------------------
+// constructors
+//
+
+Wash::Wash
+(
+    const int32_t width, const int32_t height
+) : 
+    Pattern (width, height),
+	m_step(1.0), m_scale(1.0)
+{
+}
+
+
+Wash::Wash
+(
+    const int32_t width, const int32_t height,
+	const float step, const float scale, const float angle
+) : 
+    Pattern (width, height),
+	m_step(step), m_scale(scale), m_angle(angle)
+{
+}
+
+
+//---------------------------------------------------------------------------------------------
+// destructor
+//
+
+Wash::~Wash (void)
+{
+}
+
+
+//---------------------------------------------------------------------------------------------
+// init -- reset to first frame in animation
+//
+
+void Wash::init (void)
+{
+	m_state = 0;
+}
+
+
+//---------------------------------------------------------------------------------------------
+// next -- calculate next frame in animation
+//
+
+bool Wash::next (void)
+{
+	int32_t row, col, hue;
+
+	float rads = m_angle*M_PI/180.0;
+	for (row = 0; row < m_height; row++) {
+		float x = row - 15.5;
+		for (col = 0; col < m_width; col++) {
+			float y = 15.5 - col;
+			float xp = x * cos (rads) - y * sin (rads);
+			// float yp = x * sin (rads) + y * cos (rads);
+			hue = m_state + m_scale * xp + 0.5;
+			while (hue < 0) hue += 96;
+			while (hue >= 96) hue -= 96;
+			gLevels[row][col] = translateHue (hue);
+		}
+	}
+
+	m_state = fmod ((m_state + m_step), 96.0);
+
+    return (m_state == 0);
+}
---
+++ projects/led-panel-v01/software/wash.h
@@ -0,0 +1,80 @@
+//=============================================================================================
+// LED Matrix Animated Pattern Generator
+// Copyright 2014 by Glen Akins.
+// All rights reserved.
+// 
+// Set editor width to 96 and tab stop to 4.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//=============================================================================================
+
+#ifndef __circle_h_
+#define __circle_h_
+
+class Wash : public Pattern
+{
+    public:
+        
+        // constructor
+        Wash (const int32_t width, const int32_t height);
+
+        // constructor
+        Wash 
+        (
+            const int32_t width, const int32_t height,
+            const float step, const float scale, const float angle
+        );
+
+        // destructor
+        ~Wash (void);
+
+        // reset to first frame in animation
+        void init (void);
+
+        // calculate next frame in the animation
+        bool next (void);
+
+        // get / set step, controls speed of wash
+        float getStep (void) {
+			return m_step;
+        }
+        void setStep (const float step) {
+            m_step = step;
+        }
+
+        // get / set scale, controls width of wash
+        float getScale (void) {
+			return m_scale;
+        }
+        void setScale (const float scale) {
+            m_scale = scale;
+        }
+
+		// get / set angle
+        float getAngle (void) {
+			return m_angle;
+        }
+        void setAngle (const float angle) {
+            m_angle = angle;
+        }
+
+    private:
+
+        float m_step;
+        float m_scale;
+		float m_angle;
+        float m_state;
+};
+
+#endif
