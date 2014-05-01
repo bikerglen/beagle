b4f463d5acb1da0fd81833a7235b342f825bc20d
bikerglen <glen@glen-mbpro1.local>
2014-05-01 15:24:51 -0600

    device tree setup files for use with ra2 beta ubuntu image

---
+++ projects/led-panel-v01/config-scripts/r01 ubuntu image/setup_device-tree-slow-r01.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+rm /lib/firmware/BB-BONE-LOGIBONX-SLOW.dtbo
+dtc -O dtb -o BB-BONE-LOGIBONX-SLOW.dtbo -b 0 -@ BB-BONE-LOGIBONX-SLOW.dts
+cp BB-BONE-LOGIBONX-SLOW.dtbo /lib/firmware
+sh -c "echo -4 > /sys/devices/bone_capemgr.9/slots "
+sh -c "echo -5 > /sys/devices/bone_capemgr.9/slots "
+sh -c "echo -6 > /sys/devices/bone_capemgr.9/slots "
+sh -c "echo BB-BONE-LOGIBONX:SLOW > /sys/devices/bone_capemgr.9/slots "
+cat  /sys/devices/bone_capemgr.9/slots
